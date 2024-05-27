a2c3d6c702ece1eac8ae8f5137cc6e2521e9f177
Tallal Mohar <tallalmoohar@gmail.com>
2024-05-27 00:21:26 -0700

    back arrow works . OPERATORS NEED WORK!

--- script.js
+++ script.js
@@ -1,30 +1,61 @@
+let buffer = "0";
+const display = document.querySelector(".display-screen");
+
+function rerender(){
+    display.innerText = buffer;
+}
+
 
 function buttonClick(value) {
-    if(isNaN(value)){
-        buttonWasOp(value);
+    if(isNaN(value)) {
+        handleSymbol(value);
     }
-    else{
-        buttonWasNum(value);
+    else {
+        handleNumber(value);
+    }
+    rerender();
+    
+}
+
+
+function handleNumber(value) {
+    if(buffer === "0") {
+        buffer = value;
+    }else{
+        buffer += value;
+    
     }
 }
 
-function buttonWasNum(value){
-    console.log("value is a number");
+function handleSymbol(value) {
+    if(value === "C") {
+        buffer = "0";
+    }else if(value === "=") {
+        console.log("=");
+    }else if(value === "+") {
+        console.log("+");
+    }else if(value === "-") {
+        console.log("-");
+    }else if(value === "X") {
+        console.log("x");
+    }else if(value === "%") {
+        console.log("/");
+    }else if(value === "←") {
+        if(buffer.length === 1) {
+            buffer = "0";
+        }else{
+            buffer = buffer.slice(0, buffer.length - 1);
+        }
+    }
 }
 
-function buttonWasOp(value){
-    console.log("value is an operator");
+
+function init() {
+    document.querySelector(".button-container").addEventListener("click", function(event) {
+        buttonClick(event.target.innerText);
+    });
+    rerender();
 }
+    
 
-
-
-
-
-function init(){
-   document
-  .querySelector(".button-container")
-  .addEventListener("click", function (event) {
-    buttonClick(event.target.innerText);
-});
-}
 init();
