7f8d643185c6d95e6f5901947fe8d37cd1ff936e
Tallal Mohar <tallalmoohar@gmail.com>
2024-05-27 14:16:04 -0700

    updated JS (FULLY WORKING CALCULATOR)

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 let buffer = "0";
+let runningTotal = 0;
+let previousOperator;
 const display = document.querySelector(".display-screen");
 
 function rerender(){
@@ -23,23 +25,53 @@ function handleNumber(value) {
         buffer = value;
     }else{
         buffer += value;
-    
     }
+}
+function flushOperation(intBuffer) {
+    if(previousOperator === "+") {
+        runningTotal += intBuffer;
+    }else if(previousOperator === "-") {
+        runningTotal -= intBuffer; 
+    }else if(previousOperator === "X") {
+        runningTotal *= intBuffer;
+    }else if(previousOperator === "%") {    
+        runningTotal /= intBuffer;
+    }
+}
+
+
+function handleMath(value) {
+    if(value === "0"){
+        return;
+    }
+    const intBuffer = parseInt(buffer);
+    if(runningTotal === 0) {
+        runningTotal = intBuffer;
+    }else{
+        flushOperation(intBuffer);
+    }
+    previousOperator = value;
+    buffer = "0";
 }
 
 function handleSymbol(value) {
     if(value === "C") {
         buffer = "0";
     }else if(value === "=") {
-        console.log("=");
+       if(previousOperator === null) {
+           return;
+       }
+         flushOperation(parseInt(buffer));
+         buffer = "" + runningTotal;
+         runningTotal = 0;
     }else if(value === "+") {
-        console.log("+");
+       handleMath(value);
     }else if(value === "-") {
-        console.log("-");
+        handleMath(value);
     }else if(value === "X") {
-        console.log("x");
+        handleMath(value);
     }else if(value === "%") {
-        console.log("/");
+        c=handleMath(value);
     }else if(value === "←") {
         if(buffer.length === 1) {
             buffer = "0";
