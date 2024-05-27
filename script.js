af1a843bcfff1509e35c4d39c68438a3690d3bfb
Tallal Mohar <tallalmoohar@gmail.com>
2024-05-26 22:19:51 -0700

    the buttons somewhat work with js

--- script.js
+++ script.js
@@ -1,1 +1,30 @@
-const button = document.querySelector('button');
+
+function buttonClick(value) {
+    if(isNaN(value)){
+        buttonWasOp(value);
+    }
+    else{
+        buttonWasNum(value);
+    }
+}
+
+function buttonWasNum(value){
+    console.log("value is a number");
+}
+
+function buttonWasOp(value){
+    console.log("value is an operator");
+}
+
+
+
+
+
+function init(){
+   document
+  .querySelector(".button-container")
+  .addEventListener("click", function (event) {
+    buttonClick(event.target.innerText);
+});
+}
+init();
